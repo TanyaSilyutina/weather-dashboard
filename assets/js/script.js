cb39d973725c06251105657c2dc9707d216799aa
Tanya Silyutina <48433669+TanyaSilyutina@users.noreply.github.com>
2023-04-17 00:24:17 -0700

    implemented API functions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 let locationEl = document.getElementById("location");
 let locationBtnEl = document.getElementById("locationBtn");
+let currentCityEl = document.querySelector(".currentLocation");
+let currentDateEl = document.querySelector(".currentDate");
 const previousSearches = [];
 
-async function getCoordinatesByName() {
+async function getCoordinatesByName(userInput) {
     let url = "http://api.openweathermap.org/geo/1.0/direct?";
     const params = new URLSearchParams({
-        q: locationEl.value,
+        q: userInput,
         appid: "d5039772842e201a8402984e0a92e4eb"
     });
     url += params.toString();
@@ -19,16 +21,17 @@ async function getCoordinatesByName() {
         return;
     }
     const data = await response.json();
-
-    let locationCoordinates;
-
-    // // Loop over the data, return lat lob objects
+    console.log(data[0].lat)
+    return {
+        lat: data[0].lat,
+        lon: data[0].lon,
+    };
 }
-
-async function getCoordinatesByZip(){
+// Finish this
+async function getCoordinatesByZip(userInput){
     let url = "http://api.openweathermap.org/geo/1.0/zip?zip=";
     const params = new URLSearchParams({
-        q: locationEl.value,
+        q: userInput,
         appid: "d5039772842e201a8402984e0a92e4eb"
     });
     url += params.toString();
@@ -45,26 +48,52 @@ async function getCoordinatesByZip(){
 
 }
 
-function hasNumbers(userInput){
+function getCoordsFromQuery(userInput){
     let zipCodeInput = /\d/.test(userInput);
     if (zipCodeInput) {
-        // call getCoordinatesByZip
+        return getCoordinatesByZip(userInput);
     } else {
-        // call getCoordinatesByName
+        return getCoordinatesByName(userInput);
     }
 }
 
-async function getWeather(){
-    let weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
-
+async function getWeather(e){
+    e.preventDefault();
+    let userInput = await getCoordsFromQuery(locationEl.value);
+    console.log(userInput.lat);
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?";
     const params = new URLSearchParams({
-        q: "",
+        lat: userInput.lat,
+        lon: userInput.lon,
         units: "imperial",
+        cnt: "5",
         appid: "d5039772842e201a8402984e0a92e4eb"
     });
     weatherUrl += params.toString();
     console.log(weatherUrl);
+    const response = await fetch(weatherUrl);
+    // Check if response was actually loaded successfully. If not log the whole object.
+    if (!response.ok) {
+        console.log(response);
+        return;
+    }
+    // continue assigning to display
+    // clone future time blocks
+    // make local storage
+    const data = await response.json();
+    let cityName = data.city.name;
+    console.log(cityName);
+    currentCityEl.textContent = cityName;
+    let date = data.list[0].dt_txt;
+    console.log(date);
+    currentDateEl.textContent = date;
+    let temp = data.list[0].main.temp;
+    console.log(temp);
+    let wind = data.list[0].wind.speed;
+    console.log(wind);
+    let humidity = data.list[0].main.humidity;
+    console.log(humidity);
 }
 
-locationBtnEl.addEventListener("click");
+locationBtnEl.addEventListener("click", getWeather);
 
